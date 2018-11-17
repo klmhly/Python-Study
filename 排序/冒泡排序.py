65271d95637876d1dd2f7c313a2228c67c3789b5
klmhly <2743163143@qq.com>
2018-11-17 19:19:12 +0800

    冒泡排序

---
+++ 排序/冒泡排序.py
@@ -0,0 +1,14 @@
+# 每进行一轮比较会把一个最大（或最小）的冒泡到最后一个位置
+# 那么，下一轮比较的个数会减小1个
+# 时间复杂度：O(n^2)
+def Bubble(a):
+    lens = len(a)
+    for i in range(lens-1,0,-1):
+        for j in range(i):
+            if a[j]>a[j+1]:
+                a[j],a[j+1] = a[j+1],a[j]
+    return a
+
+# 测试
+a = [1,4,8,2,5,3,7]
+print(Bubble(a))
